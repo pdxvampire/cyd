410f654e375a32796ebcd58ccbfddbbf3138386a
Jordy Simpson <vampire@rubyred.com>
2025-12-03 14:55:32 -0800

    LVGL touch using CST820

---
+++ cyd12/lv_conf.h
@@ -0,0 +1,41 @@
+#ifndef LV_CONF_H
+#define LV_CONF_H
+
+#define LV_USE_PERF_MONITOR     0
+#define LV_USE_LOG              0
+#define LV_USE_ASSERT_NULL      1
+#define LV_USE_DRAW_SW          1
+#define LV_USE_DEV_CUSTOM       0
+
+#define LV_COLOR_DEPTH          16
+#define LV_COLOR_SCREEN_TRANSP  0
+
+#define LV_TICK_CUSTOM          0
+
+#define LV_USE_USER_DATA        1
+#define LV_USE_LAYER            1
+#define LV_MEM_SIZE             (48U * 1024U)  // 48KB heap
+
+#define LV_USE_TIMER            1
+#define LV_USE_LABEL            1
+#define LV_USE_BTN              1
+#define LV_USE_DISPLAY          1
+
+// Add other widgets as needed
+#define LV_USE_BTNMATRIX        1
+#define LV_USE_SWITCH           1
+#define LV_USE_SLIDER           1
+#define LV_USE_ARC              1
+#define LV_USE_IMG              1
+
+#define LV_FS_DEFAULT_DRIVER_LETTER '\0'
+
+/** API for fopen, fread, etc. */
+#define LV_USE_FS_STDIO 1
+#if LV_USE_FS_STDIO
+#define LV_FS_STDIO_LETTER 'A'
+#define LV_FS_STDIO_PATH ""      /**< Set the working directory. File/directory paths will be appended to it. */
+#define LV_FS_STDIO_CACHE_SIZE 0 /**< >0 to cache this number of bytes in lv_fs_read() */
+#endif
+
+#endif // LV_CONF_H
