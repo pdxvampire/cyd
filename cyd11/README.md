919463a16ea9c33f46028f117c92ada90ccbdf34
Jordy Simpson <vampire@rubyred.com>
2025-12-03 13:12:58 -0800

    basic touch using CST820

---
+++ cyd11/README.md
@@ -0,0 +1,9 @@
+# cyd11 - basic touch using CST820
+
+## Setup
+In Arduino IDE turn on Serial Monitor via Tools -> Serial Monitor.
+
+## Expected Behavior When Running The Sketch
+* The display will show "Touch to draw" horizontally oriented with the USB-C port at the top.
+* Moving your finger on the display will draw blue circles along the movement path.
+* Logging will appear in the Serial Monitor.
---
+++ cyd11/cyd11.ino
@@ -0,0 +1,31 @@
+#include "LGFX_JustDisplay.h"
+#include <lvgl.h>
+#include "logging.h"
+#include "leds.h"
+#include "display.h"
+#include "touchinput.h"
+#include "code.h"
+
+void setup()
+{
+    loglevel++;  // would normally be part of the call to enterfunction for Setup()
+
+    InitializeSerialCommunication();
+    InitializeOnboardLEDs();
+    InitializeDisplay();
+
+    InitializeTouch();
+    SetupTestTouch();
+
+    // No need to blink the lights at this point
+    // unless nothing is displaying.
+    blinkflag = false;
+
+    exitfunction("setup");
+}
+
+void loop()
+{
+    HandleTouch();
+    delay(10);  // Small delay to prevent I2C flooding
+}
