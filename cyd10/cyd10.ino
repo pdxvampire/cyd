dd78b5972d0f7f88eaaafe6e505b2b45d667a6af
Jordy Simpson <vampire@rubyred.com>
2025-12-02 18:34:55 -0800

    turn on the display using LVGL on top of LovyanGFX

---
+++ cyd10/cyd10.ino
@@ -0,0 +1,62 @@
+#include "LGFX_JustDisplay.h"
+#include <lvgl.h>
+#include "logging.h"
+#include "leds.h"
+#include "display.h"
+#include "code.h"
+
+void setup()
+{
+    loglevel++;  // would normally be part of the call to enterfunction for Setup()
+
+    InitializeSerialCommunication();
+    InitializeOnboardLEDs();
+    
+    InitializeDisplay();
+    //TestDisplayJustLGFX();
+    TestDisplayLVGL();
+
+    // No need to blink the lights at this point
+    // unless nothing is displaying.
+    blinkflag = false;
+
+    exitfunction("setup");
+}
+
+void loop()
+{
+    // Seriously DO NOT ENABLE THESE or you will just
+    // get a constantly scrolling Serial Monitor
+    // window printing the entering/exiting
+    // statements
+    //
+    // enterfunction("loop");
+
+if (millis() - lastLvTick > LVGL_TICK_PERIOD)
+{
+    lv_tick_inc(LVGL_TICK_PERIOD);
+    lastLvTick = millis();
+}
+
+lv_timer_handler();
+delay(5);
+
+    // Blink so we have a visual clue that the
+    // sketch ran in case the display isn't
+    // used or isn't working.
+    if (blinkflag)
+    {
+        BlinkOnboardLEDs();
+        blinkctr++;
+
+        if (blinkctr >= 3)
+        {
+            blinkflag = false;
+        }
+
+        delay(300);
+    }
+
+    // See warning above enterfunction()
+    // exitfunction("loop");
+}
