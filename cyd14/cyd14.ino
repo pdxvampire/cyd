d8dcc4a7e5dadb23fca1ef62a204a1e88e19e61b
Jordy Simpson <vampire@rubyred.com>
2025-12-03 23:02:01 -0800

    in progress...

--- cyd14/cyd14.ino
+++ cyd14/cyd14.ino
@@ -13,31 +13,60 @@ void setup()
 
     InitializeSerialCommunication();
     InitializeOnboardLEDs();
+InitializeSDCard();
 
-    InitializeSDCard();
+
+    
     logit("####### CALL LISTDIR (1 level deep) ########");
     listDir(SD, "/", 0);
     logit("####### BACK FROM CALL LISTDIR ########");
 
-/*
-    LV_IMG_DECLARE(img_wink_png);
+
+
+if (SD.exists("/helloworld.txt"))
+{
+    Serial.println("helloworld.txt exists.");
+}
+else
+{
+    Serial.println("helloworld.txt doesn't exist.");
+}
+
+if (SD.exists("/01.png"))
+{
+    Serial.println("01.png exists.");
+}
+else
+{
+    Serial.println("01.png doesn't exist.");
+}
+
+
+InitializeDisplay();
+logit("####1");
 lv_obj_t* img;
+logit("####2");
+//img = lv_img_create(lv_scr_act());
+//lv_img_set_src(img, &img_wink_png);
+//lv_obj_align(img, LV_ALIGN_LEFT_MID, 20, 0);
 
-img = lv_img_create(lv_scr_act());
-lv_img_set_src(img, &img_wink_png);
-lv_obj_align(img, LV_ALIGN_LEFT_MID, 20, 0);
 
-img = lv_img_create(lv_scr_act());
-*/
 /* Assuming a File system is attached to letter 'A'
      * E.g. set LV_USE_FS_STDIO 'A' in lv_conf.h */
-/*
-lv_img_set_src(img, "A:/01.png");
-lv_obj_align(img, LV_ALIGN_RIGHT_MID, -20, 0);
-*/
-    SD.end();
+/*Create a new decoder and register functions */
 
-    InitializeDisplay();
+//lv_img_set_src(img, "A:/01.png");
+logit("####3");
+//lv_obj_align(img, LV_ALIGN_RIGHT_MID, 0, 0);
+//lv_obj_set_pos(img, 10, 10);
+//String content = readFile(SD, "/test.txt");
+
+//lv_obj_center(img);
+logit("####4");
+listDir(SD, "/", 0);
+logit("####5");
+    SD.end();
+    logit("####6");
 
     InitializeTouch();
     SetupTestTouch();
