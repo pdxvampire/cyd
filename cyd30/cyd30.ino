628c8f8e56f4739bb3dd17f10d056feba1c7ab99
Jordy Simpson <vampire@rubyred.com>
2025-12-19 02:05:40 -0800

    cyd30 - basic grid with separate buttons and events

---
+++ cyd30/cyd30.ino
@@ -0,0 +1,65 @@
+#include <FS.h>
+//#ifdef ESP32
+//#include "SPIFFS.h"  // ESP32 only
+//#endif
+
+#include <SPI.h>
+#include <TFT_eSPI.h>  // Hardware-specific library
+#include <lvgl.h>
+
+
+TFT_eSPI tft = TFT_eSPI();  // Invoke custom library
+
+#include "logging.h"
+#include "sdcard.h"
+#include "touchinput.h"
+#include "display.h"
+#include "code.h"
+
+// Prevent stack overflow reboot loops.
+SET_LOOP_TASK_STACK_SIZE(32 * 1024);
+
+
+
+void setup()
+{
+    InitializeSerialCommunication("SKETCH CYD29");
+
+    sdcard_setup();
+
+    InitializeDisplay();
+
+    InitializeTouch();
+
+    // draw the grid of buttons
+    // zero-based so the first time through the loop 'curpage = 0' results in 0-5,
+    // second time through the loop 'curpage = 1' results in 6-11
+    // SETTING CURPAGE TO 1 CAUSED A REBOOT LOOP SO LOOK AT THIS AGAIN WHEN HAVEN'T BEEN UP 24+ HOURS
+    for (int x = (NUMPAGEIMGS * curpage); x < (NUMPAGEIMGS * curpage) + NUMPAGEIMGS; x++)
+    {
+        // we're currently only using 10 images out of the
+        // possible 12 based on 2 pages of 6 images
+        if (x > NUMIMAGESUSED) return;
+
+        create_image_button_from_sd(x);
+    }
+
+    exitfunction("setup");
+}
+
+void loop()
+{
+    lv_display_flush_ready(disp);
+    //lv_timer_handler();
+    //lv_tick_inc(5);
+
+    //delay(5);
+    if (millis() - lastLvTick > LVGL_TICK_PERIOD)
+    {
+        lv_tick_inc(LVGL_TICK_PERIOD);  // tell LVGL how much time has passed
+        lastLvTick = millis();
+    }
+
+    lv_task_handler();        // let the GUI do its work
+    delay(LVGL_TICK_PERIOD);  // let this time pass
+}
---
+++ cyd30/touchinput.h
@@ -0,0 +1,77 @@
+#include "CST820.h"
+CST820 touch(33, 32, 25, 21);  // Touch: SDA, SCL, RST, INT
+uint16_t rawX, rawY;
+
+// Button event callback
+void HandleButtonClick(lv_event_t* e)
+{
+    if (lv_event_get_code(e) == LV_EVENT_CLICKED)
+    {
+        logit("✅ Button Clicked!");
+
+        //imageIndex++;
+        //       if (imageIndex > (MAX_IMAGES - 1)) imageIndex = 0;
+        //       displayImage(fileNames[imageIndex]);
+
+        lv_obj_t* btn = (lv_obj_t*)lv_event_get_target(e);  // Get the object that triggered the event
+                                                            // Now 'btn' points to the specific button that was clicked
+        // You can use 'btn' to identify the button
+        lv_obj_t* user_obj = (lv_obj_t*)lv_event_get_user_data(e);
+        const char* button_id = (const char*)user_obj;
+
+        logit("button id:  %s", button_id);
+
+        /*
+    
+
+    static lv_style_t style;
+    lv_style_init(&style);
+    lv_style_set_text_color(&style, lv_palette_main(LV_PALETTE_BLUE));
+    lv_style_set_text_letter_space(&style, 5);
+
+    lv_obj_t *label = lv_label_create(lv_screen_active());
+    lv_obj_add_style(label, &style, 0);
+    lv_label_set_text(label, "Hello LVGL with\nimages and styles!");
+    lv_obj_align(label, LV_ALIGN_CENTER, 0, 100);
+    lv_obj_align_to(label, imgarr, LV_ALIGN_OUT_BOTTOM_MID, 0, 30);
+*/
+        //lv_style_set_text_font(&my_style, LV_STATE_DEFAULT, &lv_font_montserrat_28);  /*Set a larger font*/
+    }
+}
+
+// Touch input for LVGL
+void HandleTouch(lv_indev_t* indev, lv_indev_data_t* data)
+{
+    uint16_t rawX, rawY;
+    if (touch.getTouch(&rawX, &rawY))
+    {
+        data->state = LV_INDEV_STATE_PRESSED;
+        
+        ///// without LVGL use this for portrait with USB at top
+        /////        data->point.x = 240 - rawX - 1;
+        /////        data->point.y = 320 - rawY - 1;
+
+        ///// with LVGL the raw coords are correct
+        data->point.x = rawX;
+        data->point.y = rawY;
+
+        logit("🖐 LVGL Touch at (%d, %d) | Raw: (%d, %d)",
+              data->point.x, data->point.y, rawX, rawY);
+    }
+    else
+    {
+        data->state = LV_INDEV_STATE_RELEASED;
+    }
+}
+
+void InitializeTouch(void)
+{
+    // Initialize touchscreen
+    touch.begin();
+    logit("🔍 Touch Chip ID: 0x%02X", touch.readChipID());
+
+    // LVGL Input device (touch)
+    lv_indev_t* indev = lv_indev_create();
+    lv_indev_set_type(indev, LV_INDEV_TYPE_POINTER);
+    lv_indev_set_read_cb(indev, HandleTouch);
+}
